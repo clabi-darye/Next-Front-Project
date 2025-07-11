61d810e4a1904de3f08ee405d4befd6ac434e8a2
clabi_darye <darye@clabi.co.kr>
2025-07-11 10:52:08 +0900

    Feat: dockerfile 추가

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "standalone",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+  compiler: {
+    styledComponents: true,
+  },
+  transpilePackages: ["three"],
 };
 
 export default nextConfig;

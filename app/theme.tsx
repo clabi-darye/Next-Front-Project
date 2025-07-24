493e10a373d89e361966049e1be657e8b05fb586
clabi_darye <darye@clabi.co.kr>
2025-07-24 15:51:02 +0900

    Feat: theme 추가

--- app/theme.tsx
+++ app/theme.tsx
@@ -2,11 +2,32 @@
 
 import { createTheme } from "@mui/material/styles";
 
+import { PaletteColorOptions } from "@mui/material/styles";
+
+declare module "@mui/material/styles" {
+  interface Palette {
+    basic: Palette["primary"];
+  }
+  interface PaletteOptions {
+    basic?: PaletteColorOptions;
+  }
+}
+
+declare module "@mui/material/Button" {
+  interface ButtonPropsColorOverrides {
+    basic: true;
+  }
+}
+
 const theme = createTheme({
   palette: {
     primary: {
       main: "#004785",
       contrastText: "#fff",
+    },
+    basic: {
+      main: "#757575",
+      contrastText: "#757575",
     },
   },
 });
